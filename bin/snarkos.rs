c8a332b8d6688fc1273f315b8dd0266ed7b9e3cb
ljedrz <ljedrz@gmail.com>
2021-11-11 09:04:53 +0100

    use jemalloc as the allocator
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- bin/snarkos.rs
+++ bin/snarkos.rs
@@ -14,6 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkOS library. If not, see <https://www.gnu.org/licenses/>.
 
+#[global_allocator]
+static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
 use snarkos::{
     cli::CLI,
     config::{Config, ConfigCli},
