bb10a46d9a0907980999d51a811f104232a6474c
ljedrz <ljedrz@users.noreply.github.com>
2025-10-09 16:27:30 +0200

    tweak: bump the peer limit to 60
    Signed-off-by: ljedrz <ljedrz@users.noreply.github.com>

--- node/router/src/heartbeat.rs
+++ node/router/src/heartbeat.rs
@@ -47,7 +47,7 @@ pub trait Heartbeat<N: Network>: Outbound<N> {
     /// The median number of peers to maintain connections with.
     const MEDIAN_NUMBER_OF_PEERS: usize = max(Self::MAXIMUM_NUMBER_OF_PEERS / 2, Self::MINIMUM_NUMBER_OF_PEERS);
     /// The maximum number of peers permitted to maintain connections with.
-    const MAXIMUM_NUMBER_OF_PEERS: usize = 21;
+    const MAXIMUM_NUMBER_OF_PEERS: usize = 60;
     /// The maximum number of provers to maintain connections with.
     const MAXIMUM_NUMBER_OF_PROVERS: usize = Self::MAXIMUM_NUMBER_OF_PEERS / 4;
     /// The amount of time an IP address is prohibited from connecting.
